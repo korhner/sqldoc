fb2284d5ad91d5fa905ee8fdf8d85e99d675947c
Ivan Korhner <ivank@nordeus.com>
2016-11-19 11:41:54 +0100

    test passed

--- tests/renderer/jinjarenderer/test_jinjarenderer.py
+++ tests/renderer/jinjarenderer/test_jinjarenderer.py
@@ -9,41 +9,47 @@ def db_metadata():
         metadata.Column('name', 'String', False, 'Name of the employee.'),
         metadata.Column('age', 'Integer', True, None)
     ]
-    employee_table = metadata.Table('employee', 'List of employees.', [employee_columns])
+    employee_table = metadata.Table('employee', 'List of employees.', employee_columns)
 
     order_columns = [
         metadata.Column('orderer_name', 'String', False, 'Name of the orderer.'),
         metadata.Column('order_id', 'String', False, 'Id of the order.')
     ]
-    order_table = metadata.Table('order', None, [order_columns])
+    order_table = metadata.Table('order', None, order_columns)
     return metadata.Database('test_db', 'A test database.', [employee_table, order_table])
 
 @fixture
 def expected_output():
-    return '''
-test_db
+    return '''test_db
 A test database.
 
-    employee
-    List of employees.
-        name
-        String
-        False
-        Name of the employee.
-        age
-        Integer
-        True
+employee
+List of employees.
 
-    order
+name
+String
+False
+Name of the employee.
 
-        orderer_name
-        String
-        False
-        Name of the orderer.
-        order_id
-        String
-        False
-        Id of the order.
+age
+Integer
+True
+None
+
+
+order
+None
+
+orderer_name
+String
+False
+Name of the orderer.
+
+order_id
+String
+False
+Id of the order.
+
 '''
 
 def test_output(tmpdir, db_metadata, expected_output):
